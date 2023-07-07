329bd2e30b9bc75ac3fdc6d7d25a2bcf8da9f9f8
kamesan1577 <kamesan1577@users.noreply.github.com>
2023-07-07 19:21:14 +0900

    画像の説明機能の実装とinit.shのバグ修正

--- init.sh
+++ init.sh
@@ -1,1 +1,7 @@
-echo $INIAD_OPENAI_API_KEY > ./iniad-openai-api-key
+if [ -n "$INIAD_OPENAI_API_KEY" ]; then
+    echo $INIAD_OPENAI_API_KEY > ./key.secret
+fi
+if [ -n "$SCENEX_API_KEY" ]; then
+    echo $SCENEX_API_KEY >  ./scenex.secret
+fi
+
